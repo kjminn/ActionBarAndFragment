4208b9a8d57fb2bb7ed13ed63dc536cb8b38e40d
kjminn <kjminn2001@gmail.com>
2020-11-05 20:43:28 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/ssu/kjminn/actionbarandfragment/MainActivity.java
@@ -0,0 +1,31 @@
+package kr.ac.ssu.kjminn.actionbarandfragment;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements ActionBar.TabListener {
+    ActionBar.Tab tabPark, tabSon, tabAhn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar bar = getSupportActionBar();
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+}
